5d132fe8fc1872c6c892f910b7bdd549d31c9868
qinsfu <44136546+qxdi@users.noreply.github.com>
2020-05-07 20:52:54 +0800

    Update index.js
    return a Generator Object

--- index.js
+++ index.js
@@ -8,40 +8,16 @@
 'use strict';
 
 module.exports = function diff(arr/*, arrays*/) {
-  var len = arguments.length;
-  var idx = 0;
-  while (++idx < len) {
-    arr = diffArray(arr, arguments[idx]);
+  let otherArr = [];
+  for (let i = 1; i < arguments.length; i++) {
+    otherArr = otherArr.concat(arguments[i]);
   }
-  return arr;
+  
+  return unique(arr, otherArr);
 };
 
-function diffArray(one, two) {
-  if (!Array.isArray(two)) {
-    return one.slice();
+function* unique(a1, a2) {
+  for (const i of a1) {
+    if (!a2.includes(i)) yield i;
   }
-
-  var tlen = two.length
-  var olen = one.length;
-  var idx = -1;
-  var arr = [];
-
-  while (++idx < olen) {
-    var ele = one[idx];
-
-    var hasEle = false;
-    for (var i = 0; i < tlen; i++) {
-      var val = two[i];
-
-      if (ele === val) {
-        hasEle = true;
-        break;
-      }
-    }
-
-    if (hasEle === false) {
-      arr.push(ele);
-    }
-  }
-  return arr;
 }
